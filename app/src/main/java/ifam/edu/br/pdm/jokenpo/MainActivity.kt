d3bf48b0c235f5990b72189706ce770cb806bbad
DaviBCSouza <contato@davibrenno.com>
2023-10-16 22:54:30 -0400

    Starting the Project

---
+++ app/src/main/java/ifam/edu/br/pdm/jokenpo/MainActivity.kt
@@ -0,0 +1,16 @@
+package ifam.edu.br.pdm.jokenpo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun toqueBotao(view: View) {
+        // Código do Botão
+    }
+}
